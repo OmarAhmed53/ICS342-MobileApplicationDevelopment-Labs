6e4b5ef98760674c3cffa21d7a96373c3df836ad
OmarAhmed53 <OmarAhmed53@gmail.com>
2023-07-17 22:58:36 -0500

    Lab 6 Work

--- app/src/main/java/com/ics342/labs/MainActivity.kt
+++ app/src/main/java/com/ics342/labs/MainActivity.kt
@@ -8,6 +8,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -16,6 +19,14 @@ import com.squareup.moshi.JsonAdapter
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.adapter
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import com.squareup.moshi.Json
+
+data class Person(
+    val id: Int,
+    @Json(name = "give_name") val givenName: String,
+    @Json(name = "family_name") val familyName: String,
+    val age: Int
+)
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,8 +40,27 @@ class MainActivity : ComponentActivity() {
                     /*
                     Display the items from the Json file in a LazyColumn
                      */
+                    MyList(data)
                 }
             }
+        }
+    }
+}
+
+@Composable
+fun PersonView(person: Person) {
+    Column {
+        Text("ID: ${person.id}")
+        Text("Name: ${person.givenName} ${person.familyName}")
+        Text("Age: ${person.age}")
+    }
+}
+
+@Composable
+fun MyList(person: List<Person>) {
+    LazyColumn {
+        items(person) { person ->
+            PersonView(person)
         }
     }
 }
@@ -43,8 +73,8 @@ private fun loadData(resources: Resources): String {
 }
 
 @OptIn(ExperimentalStdlibApi::class)
-private fun dataFromJsonString(json: String): List</* put your data class name here */> {
+private fun dataFromJsonString(json: String): List<Person> {
     val moshi: Moshi = Moshi.Builder().addLast(KotlinJsonAdapterFactory()).build()
-    val jsonAdapter: JsonAdapter<List</* put your data class name here */>> = moshi.adapter()
+    val jsonAdapter: JsonAdapter<List<Person>> = moshi.adapter()
     return jsonAdapter.fromJson(json) ?: listOf()
 }
